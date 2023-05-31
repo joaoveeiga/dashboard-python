def190a441dc2820b9fe3363b86b5373889ba8e5
Erick Goldberg <erick_goldberg@hotmail.com>
2023-05-30 21:00:35 -0300

    create real chart

--- main.py
+++ main.py
@@ -1,59 +1,59 @@
-# # Importação das bibliotecas
-# import requests
-# import json
-# import pandas as pd
-# import plotly.express as px
+# Importação das bibliotecas
+import requests
+import json
+import pandas as pd
+import plotly.express as px
 
-# # Integração da API
-# API_KEY = '9sHoGBddX4leupSZcm809zBCw88Nd5EpyPgFB04h'
-# startDate = '2023-01-01'
-# endDate = '2023-05-20'
-# response = requests.get(f"https://api.nasa.gov/DONKI/FLR?startDate={startDate}&endDate={endDate}&api_key={API_KEY}")
+# Integração da API
+API_KEY = '9sHoGBddX4leupSZcm809zBCw88Nd5EpyPgFB04h'
+startDate = '2023-01-01'
+endDate = '2023-05-20'
+response = requests.get(f"https://api.nasa.gov/DONKI/FLR?startDate={startDate}&endDate={endDate}&api_key={API_KEY}")
 
-# data = response.json()
-# dataFrame = pd.DataFrame(data)
+data = response.json()
+dataFrame = pd.DataFrame(data)
 
-# # Excluir colunas que da API que não usaremos
-# df = dataFrame.drop(['link', 'linkedEvents', 'flrID', 'instruments'], axis=1)
+# Excluir colunas que da API que não usaremos
+df = dataFrame.drop(['link', 'linkedEvents', 'flrID', 'instruments'], axis=1)
 
-# # Verificação para excluir dados com erro(Nesse caso dados que não são número para representar o tempo)
-# df = df.loc[pd.to_datetime(df['beginTime'], errors='coerce').notna()]
-# df = df.loc[pd.to_datetime(df['peakTime'], errors='coerce').notna()]
-# df = df.loc[pd.to_datetime(df['endTime'], errors='coerce').notna()]
+# Verificação para excluir dados com erro(Nesse caso dados que não são número para representar o tempo)
+df = df.loc[pd.to_datetime(df['beginTime'], errors='coerce').notna()]
+df = df.loc[pd.to_datetime(df['peakTime'], errors='coerce').notna()]
+df = df.loc[pd.to_datetime(df['endTime'], errors='coerce').notna()]
 
-# # Calculo para ver o tempo do inicio ao pico e do pico ao fim
-# df['beginToPeakTime'] = (pd.to_datetime(df['peakTime']) - pd.to_datetime(df['beginTime'])).dt.total_seconds()
-# df['peakToEndTime'] = (pd.to_datetime(df['endTime']) - pd.to_datetime(df['peakTime'])).dt.total_seconds()
+# Calculo para ver o tempo do inicio ao pico e do pico ao fim
+df['beginToPeakTime'] = (pd.to_datetime(df['peakTime']) - pd.to_datetime(df['beginTime'])).dt.total_seconds()
+df['peakToEndTime'] = (pd.to_datetime(df['endTime']) - pd.to_datetime(df['peakTime'])).dt.total_seconds()
 
-# # Verificação para excluir dados com erro(Nesse caso que o tempo levado seja inferior a 0)
-# index_negatives = df[df['beginToPeakTime'] < 0].index
-# df = df.drop(index_negatives)
-# index_negatives = df[df['peakToEndTime'] < 0].index
-# df = df.drop(index_negatives)
+# Verificação para excluir dados com erro(Nesse caso que o tempo levado seja inferior a 0)
+index_negatives = df[df['beginToPeakTime'] < 0].index
+df = df.drop(index_negatives)
+index_negatives = df[df['peakToEndTime'] < 0].index
+df = df.drop(index_negatives)
 
-# # Mostrar na tela em formato de tabelas os dados
-# df['beginTime'] = pd.to_datetime(df['beginTime'])
-# df['peakTime'] = pd.to_datetime(df['peakTime'])
-# df['endTime'] = pd.to_datetime(df['endTime'])
+# Mostrar na tela em formato de tabelas os dados
+df['beginTime'] = pd.to_datetime(df['beginTime'])
+df['peakTime'] = pd.to_datetime(df['peakTime'])
+df['endTime'] = pd.to_datetime(df['endTime'])
 
-# df['sameDay'] = (df['beginTime'].dt.date == df['peakTime'].dt.date) & (df['beginTime'].dt.date == df['endTime'].dt.date)
+df['sameDay'] = (df['beginTime'].dt.date == df['peakTime'].dt.date) & (df['beginTime'].dt.date == df['endTime'].dt.date)
 
-# # Calcular o dia que mais se repete(moda) 
-# same_day_counts = df['sameDay'].value_counts()
-# df = df.loc[df['sameDay'] == True]
-# print(same_day_counts)
+# Calcular o dia que mais se repete(moda) 
+same_day_counts = df['sameDay'].value_counts()
+df = df.loc[df['sameDay'] == True]
+print(same_day_counts)
 
-# # Calculo para ver em minutos o tempo que foi levado
-# beginToPeakTimeInMinutes = df['beginToPeakTime'].mean()/60
-# peakToEndTimeInMinutes = df['peakToEndTime'].mean()/60
+# Calculo para ver em minutos o tempo que foi levado
+beginToPeakTimeInMinutes = df['beginToPeakTime'].mean()/60
+peakToEndTimeInMinutes = df['peakToEndTime'].mean()/60
 
-# print(beginToPeakTimeInMinutes, peakToEndTimeInMinutes)
-# df
+print(beginToPeakTimeInMinutes, peakToEndTimeInMinutes)
+df
 
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
-# df['date'] = df['beginTime'].dt.date
-# flr_counts = df['date'].value_counts().sort_index()
+df['date'] = df['beginTime'].dt.date
+flr_counts = df['date'].value_counts().sort_index()
 
 # plt.figure(figsize=(12, 6))
 # plt.bar(flr_counts.index, flr_counts.values)
@@ -93,15 +93,34 @@ import io
 import matplotlib.pyplot as plt
 
 def generate_plot():
-    # Dados para o gráfico
-    x = [1, 2, 3, 4, 5]
-    y = [2, 4, 6, 8, 10]
+    # # Dados para o gráfico
+    # x = [1, 2, 3, 4, 5]
+    # y = [2, 4, 6, 8, 10]
 
-    # Criar o gráfico
-    plt.plot(x, y)
-    plt.xlabel('X')
-    plt.ylabel('Y')
-    plt.title('Gráfico de exemplo')
+    # # Criar o gráfico
+    # plt.plot(x, y)
+    # plt.xlabel('X')
+    # plt.ylabel('Y')
+    # plt.title('Gráfico de exemplo')
+
+    # # Converter o gráfico em objeto bytes
+    # buffer = io.BytesIO()
+    # plt.savefig(buffer, format='png')
+    # buffer.seek(0)
+
+    df['date'] = df['beginTime'].dt.date
+    flr_counts = df['date'].value_counts().sort_index()
+
+    # Criar o gráfico de barras
+    plt.bar(flr_counts.index, flr_counts.values)
+
+    # Configurar o título e os rótulos dos eixos
+    plt.title('FLR Counts by Day')
+    plt.xlabel('Date')
+    plt.ylabel('FLR Count')
+
+    # Girar os rótulos do eixo x em 45 graus
+    plt.xticks(rotation=45)
 
     # Converter o gráfico em objeto bytes
     buffer = io.BytesIO()
